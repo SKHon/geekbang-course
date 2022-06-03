e4e5e0c9e9ce6a50fc1c7bd87e072251ca631d13
刘江虹 <liujianghong@bytedance.com>
2022-06-03 16:54:19 +0800

    feat: add

---
+++ course-03/example/src/App.js
@@ -0,0 +1,15 @@
+
+import Home from './components/Home';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div >
+      app
+      <Home />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ course-03/example/src/components/Footer.js
@@ -0,0 +1,7 @@
+import Message from './Message';
+export default () => {
+  return <div> 
+    <Message />
+    this footer
+  </div>
+}
