258a1d9aa62ff5525c70db8c3491209e53591f96
刘江虹 <liujianghong@bytedance.com>
2022-06-03 17:37:39 +0800

    feat: chnge

--- course-03/webpack-plugin/index.js
+++ course-03/webpack-plugin/index.js
@@ -11,42 +11,42 @@ module.exports = class DemoPlugin {
   }
   apply(compiler) {
     compiler.hooks.normalModuleFactory.tap('DepAnalysisPlugin', (nmf) => {
-      // nmf.hooks.afterResolve.tapAsync('DepAnalysisPlugin', (result, callback) => {
+      nmf.hooks.afterResolve.tap('DepAnalysisPlugin', (result) => {
           
-      //     const resourceResolveData = result?.createData?.resourceResolveData;
-      //     const curFile = resourceResolveData?.path;
-      //     const parentFile = resourceResolveData?.context?.issuer
+          const resourceResolveData = result?.createData?.resourceResolveData;
+          const curFile = resourceResolveData?.path;
+          const parentFile = resourceResolveData?.context?.issuer
           
           
-      //     // 对于合法文件进行分析
-      //     if(curFile && checkFile(curFile)) {
-      //       console.log(parentFile);
-      //       console.log(curFile);
-      //       // console.log(resourceResolveData);
-      //       console.log('\n\n');
+          // 对于合法文件进行分析
+          if(curFile && checkFile(curFile)) {
+            console.log(parentFile);
+            console.log(curFile);
+            // console.log(resourceResolveData);
+            console.log('\n\n');
             
 
-      //       // 如果有父级文件，则直接添加
-      //       if(parentFile) {
-      //         const curNode = BFSTravel(this.dependenciesTree, parentFile);
-      //         if(curNode) {
-      //           curNode.deps.push({
-      //             path: curFile,
-      //             deps: []
-      //           })
-      //         } 
-      //       } else { // 说明是根路径
-      //         this.dependenciesTree = {
-      //           path: curFile,
-      //           deps: []
-      //         }
-      //       }
-      //       // console.log(JSON.stringify(this.dependenciesTree), '\n');
-      //       fs.writeFileSync('student-2.json', JSON.stringify(this.dependenciesTree));
-      //     } 
-      //     callback()
-      //   }
-      // )
+            // 如果有父级文件，则直接添加
+            if(parentFile) {
+              const curNode = BFSTravel(this.dependenciesTree, parentFile);
+              if(curNode) {
+                curNode.deps.push({
+                  path: curFile,
+                  deps: []
+                })
+              } 
+            } else { // 说明是根路径
+              this.dependenciesTree = {
+                path: curFile,
+                deps: []
+              }
+            }
+            // console.log(JSON.stringify(this.dependenciesTree), '\n');
+            fs.writeFileSync('student-2.json', JSON.stringify(this.dependenciesTree));
+          } 
+          // callback()
+        }
+      )
 
     });
    
